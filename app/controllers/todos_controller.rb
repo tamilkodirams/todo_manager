6ec4f461edf22443653f057af507cf63462695a7
tamil <hod.mca@giet.ac.in>
2021-09-29 16:59:18 +0530

    design-based HTML & CSS with ERB

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,7 +9,8 @@ class TodosController < ApplicationController
   def show
     id = params[:id]
     todo = Todo.find(id)
-    render plain: todo.to_pleasant_string.to_s
+    render "todos"
+    #render plain: todo.to_pleasant_string.to_s
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get "todos",to:"todos#index"
-  #resources :todos
+  #get "todos",to:"todos#index"
+  get "/" => "home#index"
+  resources :todos
   #post "users/login", to: "users#login"
   #resources :users
 end

327b6f04f388c41a10975f81bfe23570dd8c78fa
tamil <hod.mca@giet.ac.in>
2021-10-22 16:22:57 +0530

    Storing passwords with bcrypt and password digests

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,5 @@
 class Todo < ActiveRecord::Base
+  belongs_to :user
 
   def due_today?
     due_date == Date.today
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #get "todos",to:"todos#index"
-  #get "/" => "home#index"
+  get "/" => "home#index"
   resources :todos
   #post "users/login", to: "users#login"
-  #resources :users
+  resources :users
 end
